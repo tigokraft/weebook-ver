028704e7a2ee34a86de67682f61dfea3c282feba
tigokraft <77751777+tigokraft@users.noreply.github.com>
2024-04-27 22:33:26 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Notes Picker",
+  title: "WeeBook",
   description: "Created by Ani - Solve It Out",
 };
 
