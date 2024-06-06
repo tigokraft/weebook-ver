99562910acd02d42b808b66b28c7825baff9eea5
tigokraft <77751777+tigokraft@users.noreply.github.com>
2024-06-06 13:16:01 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NiggaBook",
+  title: "WeeBook",
   description: "WeeBook by tigo",
 };
 
